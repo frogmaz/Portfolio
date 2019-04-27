ebb052035b168a680feb234d9b6997bb099b66fd
frogmaz <frogs.n.grandmas@gmail.com>
2019-04-27 10:56:25 +0200

    fixed overflow of layout

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default () =>
   <div className="header">
     <h1 id="milos">milos</h1>
     <h3>front-end web developer</h3>
-    <div id="trougao" className="navOff">
+    <div id="trougao" className="Off">
       <div id="crta" />
     </div>
     <Nav />
--- src/components/nav.js
+++ src/components/nav.js
@@ -3,8 +3,23 @@ import { Link } from 'gatsby'
 
 import navStyles from './nav.module.scss'
 
+// // toggle menu
+// const nav = document.querySelector("#nav");
+// const trougao = document.querySelector("#trougao");
+//
+// function toggleClass(el){
+//   console.log("opalio");
+//   if(el.className === 'on'){
+//     el.className = 'off';
+//   }else{
+//     el.className = 'on';
+//   }
+// }
+//
+// trougao.addEventListener('click', function(){toggleClass(nav); toggleClass(trougao)});
+
 export default () =>
-  <ul id="nav" className="navOff">
+  <ul id="nav" className="off">
     <Link activeClassName={navStyles.activeNavItem} to="/">
       <h1>
         home
