cc1f22949453c97ce20cc4789fd091dc684a9249
frogmaz <frogs.n.grandmas@gmail.com>
2019-05-07 16:54:49 +0200

    replaced plygons with png-s for ME

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+
 //uvodjenje slike (da bi kasnijim build-om sve leglo na mesto)
 import Portret from '../images/portret.png'
-
+import Trougao1 from '../images/trougao1.png'
 export default () =>
   <div className="footer">
     <div className="img-container">
-      <div className="img-triangle" />
+      <img src={Trougao1} className="img-triangle" alt="drop-down menu triangle"/>
       <Link to="/contact" >
-        <img src={Portret} alt="pixeleted portret" className="portret"/>
+        <img src={Portret} className="portret" alt="pixeleted portret" />
       </Link>
       <p>&#169;Miloš</p>
     </div>
