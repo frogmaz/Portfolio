15d87d5edff4879a1ab64126412b9e0e1ac49793
obrovacm <frogs.n.grandmas@gmail.com>
2019-11-30 12:34:20 +0100

    projects JS & portret CSS

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,38 +1,71 @@
-import React from "react"
+import React, { Component } from "react"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+import Layout from "../components/layout"
+import Image from "../components/image"
 
+export default class projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 5,
+        link: "https://hillshearts.netlify.com/",
+        imgName: "hillshearts",
+        description:
+          "E-commerce website built with Gatsby, Bootstrap, Contentful, Formspree and Snipcart.",
+      },
+      {
+        id: 4,
+        link: "https://obrovacm.github.io/booklist/",
+        imgName: "booklist",
+        description:
+          "Simple 'booklist' app that's using local storage to function. Built with VanilaJS & Bootstrap.",
+      },
+      {
+        id: 3,
+        link: "https://sportsko-udruzenje.netlify.com/",
+        imgName: "sportska-asocijacija",
+        description:
+          "Static Gatsby web page. Adaptive to different screen sizes, drop menu on small screens, few simple animations, file downloading, gallery,google maps iframe, svg icons...",
+      },
+      {
+        id: 2,
+        link: "https://obrovacm.github.io/Z-uraditi-listak/index.html",
+        imgName: "notesNew",
+        description:
+          "Classic to-do/notes app, with a few extra functionalities: multiple tabs, search through the list of notes and delete all marked notes.",
+      },
+      {
+        id: 1,
+        link: "https://quietplace.netlify.com/",
+        imgName: "quietplace",
+        description:
+          "Simple, company-looking, website. It's fully responsive (adapts to different screen sizes), it has simple SVG-s, an animated logo and a hamburger drop-down menu for small screens.",
+      },
+    ],
+  }
 
-export default () =>
-  <Layout page="my projects">
-    <div className="projects-page">
-      <div className="project">
-        <a href="https://sportsko-udruzenje.netlify.com/"  target="_blank" rel="noopener noreferrer">
-          <div className="project-img">
-            <Image filename="sportsko-udruzenje" />
-          </div>
-          <p>Static Gatsby web page. Adaptive to different screen sizes, drop menu on small screens, few simple animations, file downloading, gallery, google maps iframe, svg icons...</p>
-        </a>
-      </div>
-
-      <div className="project">
-        <a href="https://quietplace.netlify.com/"  target="_blank" rel="noopener noreferrer">
-          <div className="project-img">
-            <Image filename="quietplace" />
-          </div>
-          <p>Simple, company-looking, website. It's fully responsive (adapts to different screen sizes), it has simple SVG-s, an animated logo and a hamburger drop-down menu for small screens.</p>
-        </a>
-      </div>
-
-      <div className="project">
-        <a href="https://obrovacm.github.io/Z-uraditi-listak/index.html"  target="_blank" rel="noopener noreferrer">
-          <div className="project-img">
-            <Image filename="notesNew" />
-          </div>
-          <p>Classic to-do/notes app, with a few extra functionalities: multiple tabs, search through the list of notes and delete all marked notes.</p>
-        </a>
-      </div>
-
-    </div>
-  </Layout>
+  render() {
+    return (
+      <Layout page="my projects">
+        <div className="projects-page">
+          {this.state.projects.map(project => {
+            return (
+              <div className="project" key={project.id}>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="project-img">
+                    <Image filename={project.imgName} />
+                  </div>
+                  <p>{project.description}</p>
+                </a>
+              </div>
+            )
+          })}
+        </div>
+      </Layout>
+    )
+  }
+}
