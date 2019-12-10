56edc1ff805c1981ae427463a999ddaa994bb7d8
milos <obrovac.milos@gmail.com>
2019-12-10 16:20:38 +0100

    slider at 50%

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,6 +37,17 @@ export default props => (
     <Header />
     <div className="page-title">
       <h1>{props.page}</h1>
+      <span className="sliders">
+        <span className="slider s1">
+          <span className="slider s2">
+            <span className="slider s3">
+              <span className="slider s4">
+                <span className="slider s5"></span>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
     </div>
     <PageTransition
       defaultStyle={{
