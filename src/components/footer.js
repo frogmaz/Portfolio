a76894f28f0c3024faa30327e57a839cba2b92c1
milos <obrovac.milos@gmail.com>
2019-12-11 10:11:50 +0100

    cleand up from comments & unused code

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-// import Image from "../components/image"
 
 import Portret from "../images/portret.png"
 import Trougao1 from "../images/trougao1.png"
@@ -14,9 +13,6 @@ export default () => (
         alt="drop-down menu triangle"
       />
       <Link to="/contact">
-        {/* <div className="portret-container">
-          <Image fileName="portret" className="portret" />
-        </div> */}
         <img src={Portret} className="portret" alt="pixeleted portret" />
       </Link>
       <p>&#169;miloš_dev</p>
