8c2d60a8058f656d5d7de5ce9f02079db98f8a80
obrovacm <frogs.n.grandmas@gmail.com>
2020-01-01 23:56:56 +0100

    a comment for projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 
 export default class projects extends Component {
+  // dodati filter za projekte po kateogrijama i napraviti klasu za projekat
   state = {
     projects: [
       {
